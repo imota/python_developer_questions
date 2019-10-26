972675291280a7c2dcd4ef186a70f1c958f77415
= <mota.icaro@gmail.com>
2019-10-26 12:55:13 -0300

    simple LRU cache implemented

--- QuestionC.py
+++ QuestionC.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python3
 
+'''
+TODO:
+1 - Time expiration
+2 - Resilient to network failures or crashes.
+3 - Near real time replication of data across Geolocation. Writes need to be in real time.
+4 - Data consistency across regions
+5 - Locality of reference, data should almost always be available from the closest region
+6 - Flexible Schema
+'''
 class LRUCache():
     def __init__(self, cache_capacity):
         self.cached_dictionary = {}
         self.cached_list = DoubleLinkedList()
         self.cache_count = 0
         self.cache_capacity = cache_capacity
+
+    def _free_cache(self):
+        removed_item = self.cached_list.pop()
+        if removed_item != None:
+            self.cached_dictionary.pop(removed_item.key, None)
+            self.cache_count -= 1
+
+    def get_free_capacity(self):
+        return self.cache_capacity - self.cache_count
+
+    def get_cache_count(self):
+        return self.cache_count
 
     def get(self, key):
         if not key in self.cached_dictionary:
@@ -15,7 +36,16 @@ class LRUCache():
         return item.value
 
     def put(self, key, value):
-        pass
+        if key in self.cached_dictionary:
+            item = self.cached_dictionary[key]
+            item.value = value
+            self.cached_list.move_to_start(item)
+        else:
+            if self.cache_count == self.cache_capacity:
+                self._free_cache()
+            new_item = self.cached_list.add_item(key, value)
+            self.cached_dictionary[key] = new_item
+            self.cache_count += 1
 
 class DoubleLinkedList():
     def __init__(self):
@@ -27,13 +57,18 @@ class DoubleLinkedList():
     def move_to_start(self, node): #error trying to remove start or end
         self.remove(node)
         node.remove()
-        self.add(node)
+        self._add_node(node)
     
     def remove(self, node):
         node.remove()
         self.count -= 1
 
-    def add(self, node):
+    def add_item(self, key, item):
+        new_node = DoubleLinkedNode(key = key, value = item)
+        self._add_node(new_node)
+        return new_node
+
+    def _add_node(self, node):
         node.prev = self.start
         node.next = self.start.next
         self.start.next.prev = node
@@ -41,11 +76,15 @@ class DoubleLinkedList():
         self.count += 1
 
     def pop(self):
-        if self.end.prev != self.start:
-            self.remove(self.end.prev)
+        item = self.end.prev
+        if item != self.start:
+            self.remove(item)
+            return item
+        return None
 
 class DoubleLinkedNode():
-    def __init__(self, value = None, prev = None, next = None):
+    def __init__(self, key = None, value = None, prev = None, next = None):
+        self.key = key #we store the key to make deletion from the dictionary based on the node faster
         self.value = value
         self.prev = prev
         self.next = next
--- QuestionC_test.py
+++ QuestionC_test.py
@@ -3,48 +3,90 @@
 import pytest
 from QuestionC import DoubleLinkedList
 from QuestionC import DoubleLinkedNode
+from QuestionC import LRUCache
 
-def test1():
+def test_DoubleLinkedList_1():
     linked_list = DoubleLinkedList()
-    linked_list.add(DoubleLinkedNode(value = 1))
+    linked_list.add_item(key = 1, item = 1)
     assert linked_list.count == 1
 
-def test2():
+def test_DoubleLinkedList_2():
     linked_list = DoubleLinkedList()
-    linked_list.add(DoubleLinkedNode(value = 5))
+    linked_list.add_item(key = 1, item = 5)
     assert linked_list.start.next.value == 5
 
-def test3():
+def test_DoubleLinkedList_3():
     linked_list = DoubleLinkedList()
-    linked_list.add(DoubleLinkedNode(value = 5))
+    linked_list.add_item(key = 1, item = 5)
     linked_list.pop()
     assert linked_list.count == 0
 
-def test4():
+def test_DoubleLinkedList_4():
     linked_list = DoubleLinkedList()
-    linked_list.add(DoubleLinkedNode(value = 5))
+    linked_list.add_item(key = 1, item = 5)
     linked_list.pop()
     assert linked_list.start.next == linked_list.end
 
-def test5():
+def test_DoubleLinkedList_5():
     linked_list = DoubleLinkedList()
     linked_list.pop()
     assert linked_list.start.next == linked_list.end
 
-def test6():
+def test_DoubleLinkedList_6():
     linked_list = DoubleLinkedList()
-    linked_list.add(DoubleLinkedNode(value = 1))
-    linked_list.add(DoubleLinkedNode(value = 3))
+    linked_list.add_item(key = 1, item = 1)
+    linked_list.add_item(key = 2, item = 3)
     assert linked_list.count == 2
 
-def test7():
+def test_DoubleLinkedList_7():
     linked_list = DoubleLinkedList()
-    linked_list.add(DoubleLinkedNode(value = 1))
-    linked_list.add(DoubleLinkedNode(value = 3))
+    linked_list.add_item(key = 1, item = 1)
+    linked_list.add_item(key = 2, item = 3)
     assert linked_list.start.next.next.value == 1
 
-def test8():
+def test_DoubleLinkedList_8():
     linked_list = DoubleLinkedList()
-    linked_list.add(DoubleLinkedNode(value = 1))
-    linked_list.add(DoubleLinkedNode(value = 3))
+    linked_list.add_item(key = 1, item = 1)
+    linked_list.add_item(key = 2, item = 3)
     assert linked_list.end.prev.prev.value == 3
+
+def test_DoubleLinkedList_9():
+    linked_list = DoubleLinkedList()
+    linked_list.add_item(key = 1, item = 1)
+    linked_list.add_item(key = 1, item = 3)
+    assert linked_list.count == 2
+
+def test_LRUCache_1():
+    cache = LRUCache(cache_capacity = 1)
+    cache.put(1, 5)
+    assert cache.get(1) == 5
+
+def test_LRUCache_2():
+    cache = LRUCache(cache_capacity = 1)
+    cache.put(1, 5)
+    cache.put(2, 3)
+    assert cache.get(1) == None
+
+def test_LRUCache_3():
+    cache = LRUCache(cache_capacity = 1)
+    cache.put(1, 5)
+    cache.put(2, 3)
+    assert cache.get(2) == 3
+
+def test_LRUCache_4():
+    cache = LRUCache(cache_capacity = 1)
+    cache.put(1, 5)
+    cache.put(1, 3)
+    assert cache.get(1) == 3
+
+def test_LRUCache_5():
+    cache = LRUCache(cache_capacity = 1)
+    cache.put(1, 5)
+    cache.put(2, 3)
+    assert cache.get_cache_count() == 1
+
+def test_LRUCache_2():
+    cache = LRUCache(cache_capacity = 1)
+    cache.put(1, 5)
+    cache.put(2, 3)
+    assert cache.get(2) == 3
