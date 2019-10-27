24fb0744c37bcb1f08ca120f299d64d93dac3879
= <mota.icaro@gmail.com>
2019-10-27 10:51:06 -0300

    renamed the tests

--- QuestionA_test.py
+++ QuestionA_test.py
@@ -6,46 +6,49 @@ import unittest
 
 class TestStringMethods(unittest.TestCase):
 
-    def test1(self):
+    def test_should_overlap_segment1_starts_to_the_left(self):
         self.assertTrue(does_overlap(1,5,2,6))
 
-    def test2(self):
+    def test_shouldnt_overlap_segment2_start_exactly_at_end_of_segment1(self):
         self.assertFalse(does_overlap(2,6,6,8))
 
-    def test3(self):
+    def test_should_overlap_segment1_starts_to_the_right(self):
         self.assertTrue(does_overlap(2,6,1,5))
 
-    def test4(self):
+    def test_shouldnt_overlap_segment2_ends_exactly_at_start_of_segment1(self):
         self.assertFalse(does_overlap(6,8,2,6))
 
-    def test5(self):
+    def test_should_overlap_equal_segments(self):
         self.assertTrue(does_overlap(6,8,6,8))
 
-    def test6(self):
+    def test_should_overlap_segment1_starts_to_the_left_negative_numbers(self):
         self.assertTrue(does_overlap(-10,-1,-5,0))
 
-    def test7(self):
+    def test_should_overlap_segment1_starts_to_the_right_negative_numbers(self):
         self.assertTrue(does_overlap(-5,0,-1,-10))
 
-    def test8(self):
+    def test_should_overlap_segment1_starts_to_the_left_inverted_segments(self):
         self.assertTrue(does_overlap(5,1,6,2))
 
-    def test9(self):
+    def test_should_overlap_segment2_inside_segment1(self):
         self.assertTrue(does_overlap(1,5,2,3))
 
-    def test10(self):
+    def test_should_overlap_segment1_inside_segment2(self):
         self.assertTrue(does_overlap(2,3,1,5))
 
-    def test11(self):
+    def test_should_overlap_segment1_starts_to_the_right_inverted_segments(self):
+        self.assertTrue(does_overlap(2,3,1,5))
+
+    def test_should_overlap_segment2_inside_segment1_negative_numbers(self):
         self.assertTrue(does_overlap(-1,-5,-2,-3))
 
-    def test12(self):
+    def test_should_overlap_segment1_inside_segment2_negative_numbers(self):
         self.assertTrue(does_overlap(-2,-3,-1,-5))
 
-    def test13(self):
+    def test_shouldnt_overlap_segment1_negative_segment2_positive(self):
         self.assertFalse(does_overlap(-1,-5,2,3))
 
-    def test14(self):
+    def test_shouldnt_overlap_segment1_positive_segment2_negative(self):
         self.assertFalse(does_overlap(-2,-3,1,5))
 
 if __name__ == '__main__':
--- QuestionC_test.py
+++ QuestionC_test.py
@@ -7,63 +7,57 @@ import unittest
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_LRUCache_1(self):
+    def test_LRUCache_put_1(self):
         cache = LRUCache(capacity = 1)
         cache.put(1, 5)
         self.assertEqual(cache.get(1), 5)
 
-    def test_LRUCache_2(self):
+    def test_LRUCache_put_2_capacity_1_shouldnt_find_first(self):
         cache = LRUCache(capacity = 1)
         cache.put(1, 5)
         cache.put(2, 3)
         self.assertEqual(cache.get(1), None)
 
-    def test_LRUCache_3(self):
+    def test_LRUCache_put_2_capacity_1_find_second(self):
         cache = LRUCache(capacity = 1)
         cache.put(1, 5)
         cache.put(2, 3)
         self.assertEqual(cache.get(2), 3)
 
-    def test_LRUCache_4(self):
+    def test_LRUCache_put_2_same_key(self):
         cache = LRUCache(capacity = 1)
         cache.put(1, 5)
         cache.put(1, 3)
         self.assertEqual(cache.get(1), 3)
 
-    def test_LRUCache_5(self):
+    def test_LRUCache_put_2_capacity_1_cached_count_should_be_1(self):
         cache = LRUCache(capacity = 1)
         cache.put(1, 5)
         cache.put(2, 3)
         self.assertEqual(cache.get_cache_count(), 1)
 
-    def test_LRUCache_6(self):
-        cache = LRUCache(capacity = 1)
-        cache.put(1, 5)
-        cache.put(2, 3)
-        self.assertEqual(cache.get(2), 3)
-
-    def test_LRUCache_7(self):
+    def test_LRUCache_put_2_capacity_1_delete_1_count_should_be_0(self):
         cache = LRUCache(capacity = 1)
         cache.put(1, 5)
         cache.put(2, 3)
         cache.delete(2)
         self.assertEqual(cache.get_cache_count(), 0)
 
-    def test_LRUCache_8(self):
+    def test_LRUCache_put_2_capacity_1_delete_1_get_shouldnt_find_anything(self):
         cache = LRUCache(capacity = 1)
         cache.put(1, 5)
         cache.put(2, 3)
         cache.delete(2)
         self.assertEqual(cache.get(2), None)
 
-    def test_LRUCache_9(self):
+    def test_LRUCache_delete_already_removed_element(self):
         cache = LRUCache(capacity = 1)
         cache.put(1, 5)
         cache.put(2, 3)
         cache.delete(1)
         self.assertEqual(cache.get_cache_count(), 1)
 
-    def test_LRUCache_10(self):
+    def test_LRUCache_get_after_time_expiration_shouldnt_find(self):
         cache = LRUCache(capacity = 1, expiration_time = timedelta(milliseconds=1))
         cache.put(1, 5)
         time.sleep(0.1)
